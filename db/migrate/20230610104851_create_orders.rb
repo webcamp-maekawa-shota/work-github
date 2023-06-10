33fcc99c1a22f5acedb56ff52b75cb6be909dbdc
EC2 Default User <ec2-user@ip-172-31-40-200.ap-northeast-1.compute.internal>
2023-06-10 11:16:28 +0000

    Add Table

---
+++ db/migrate/20230610104851_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.string :shipping_address, null: false
+      t.string :shipping_postal_code, null: false
+      t.string :shipping_name, null: false
+      t.integer :payment_method, null: false, default: 0
+      t.integer :postage, null: false
+      t.integer :billing_amount, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_10_094950) do
+ActiveRecord::Schema.define(version: 2023_06_10_111112) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "introduction", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
+    t.integer "order_price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "shipping_address", null: false
+    t.string "shipping_postal_code", null: false
+    t.string "shipping_name", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "postage", null: false
+    t.integer "billing_amount", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
